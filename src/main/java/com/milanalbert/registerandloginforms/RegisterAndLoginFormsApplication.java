6ae54178eb609131f833c8d1e86d383a2dedfe31
ovp007 <m.albertbb@gmail.com>
2022-03-03 08:42:00 +0100

    init commit

---
+++ src/main/java/com/milanalbert/registerandloginforms/RegisterAndLoginFormsApplication.java
@@ -0,0 +1,13 @@
+package com.milanalbert.registerandloginforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterAndLoginFormsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterAndLoginFormsApplication.class, args);
+    }
+
+}
